d65f7a3a3f5a1cd9e353f44b28630a6e7d99a43b
KamoMaimela <2103503@students.wits.ac.za>
2021-08-21 10:25:42 +0200

    Update

--- Assets/Scripts/Eat.cs
+++ Assets/Scripts/Eat.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Eat : MonoBehaviour
 {
@@ -9,10 +10,37 @@ public class Eat : MonoBehaviour
 
     public Text dialogueText;
 
+    public float seconds = 0f;
+
+    public bool HasEaten = false;
+
+    public GameObject Food;
+
 
     void OnMouseDown() //Clicking on the food item makes a chewing sound.
     {
         EatSound.Play();
+        HasEaten = true;
     }
 
+    void Update()
+    {
+        //adding a timer so that once the player has eaten, food item disappears
+        
+        if (HasEaten)
+        {
+            seconds += Time.deltaTime;
+        }
+        
+        if (seconds >= 5)
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+        }
+
+        if (seconds >= 9) //Change scene
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }
